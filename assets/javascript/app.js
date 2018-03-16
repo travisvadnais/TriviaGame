0c2df8079c1c79dccfb7553498ff37a83e320c60
Travis Vadnais <travisvadnais@gmail.com>
2018-03-15 22:11:18 -0400

    semi-final; may still style a bit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
         new quizQuestion("Question_9", "What was Haley Joe Osmont's 'Sixth Sense'?", ["Telepathy", "Time Travel", "Truth Serum", "Seeing the Dead"], 3, "Q9"),
         new quizQuestion("Question_10", "Which film is most likely to make you avoid going to the beach?", ["It", "Children of the Corn", "Jaws", "Cujo"], 2, "Q10")
     ];
-    //array to hold the 'hard' questions
+    //array to hold the 'hard' questions - not built into HW yet.  Will incorporate later.
     var quizQuestionsHard = [
         new quizQuestion("Question_1", "Tobin Bell, who is best known for his role as 'Jigsaw' in the 'Saw' franchise, played a record store owner in one episode of which popular 90's sitcom?", ["Friends", "Step-by-Step", "Seinfeld", "Frasier"], 2, "Q1"),
         new quizQuestion("Question_2", "The Tales from the Crypt TV Series produced two big-screen theatrical releases.  What was the title of the little-known third installment?", ["House of 1,000 Corpses", "Sanctuary", "Nine Lives", "Ritual"], 3, "Q2"),
@@ -39,18 +39,6 @@ $(document).ready(function () {
         new quizQuestion("Question_10", "In the 1990 movie 'Tremors' with Kevin Bacon, what alerted the giant worms to the characters' whereabouts?", ["Smell of Barbecue", "Radio Waves", "Microwaves", "Vibrations"], 3, "Q10")
     ];
 
-    //variable used for the timer/countdown
-    var timer;
-
-    //Arrays to hold the actual correct answers and user's quiz answers
-    var correctAnswers = [];
-    var userAnswers = [];
-    
-
-    //Answer counters
-    var answersCorrect = 0;
-    var answersWrong = 0;
-    
     //This function formats the questions for us.  Instead of following object notation for 20 questions, we can use the function to format them for us just by feeding it inputs (which are the 'new' items above)
     function quizQuestion(questionNumber, question, choices, correctAnswer, name) {
         this.questionNumber = questionNumber;
@@ -59,6 +47,39 @@ $(document).ready(function () {
         this.correctAnswer = correctAnswer;
         this.name = name;
     }
+    //Array holding all the different outcomes that will populate on the calculation screen
+    var outcomeAnalysis = [
+        new outcome("Leprechaun", "Really, you are the least scary horror villain in history.  Your performance in this quiz was almost as pathetic as the idea of you as a bad guy.  We can only hope there aren't 6 sequels of YOU", "assets/images/outcome0.png"),
+        new outcome("Corn Child", "Honestly, the only thing scarier than a dork in a sombrero is your performance in this quiz.", "assets/images/outcome1.png"),
+        new outcome("Bad Moon Werewolf", "What the hell is that?!  No, not the image; your performance.  Come back when you've learned how to use the Google machine", "assets/images/outcome2.png"),
+        new outcome("The Fisherman", "I know what you did last summer . . . . literally everything but watch horror movies.", "assets/images/outcome3.png"),
+        new outcome("Feardotcom", "Yes, that's right - your performance doesn't even deserve the honor of a villain; you're just an evil website.  Maybe next time instead of using the internet to scare people to death, you can use it to, I don't know, look for horror quiz answers.", "assets/images/outcome4.png"),
+        new outcome("The Collector", "Okay, so you weren't horrible, but you definitely weren't great.  Parts of your performance were brilliant, and some made you just shake your head.  Keep trying.", "assets/images/outcome5.png"),
+        new outcome("Negan", "You're not horrible, you're not great; you're just kind of there.  The most entertaining part of your performance is how many curse words you say.  Much like me when reading your answers.", "assets/images/outcome6.png"),
+        new outcome("Pinhead", "A certified badass - one of the most depraved villains of all time.  Unfortunately, this franchise really trailed off after awhile, much like your performance on this quiz", "assets/images/outcome7.png"),
+        new outcome("Annie Wilkes", "Now we're getting somewhere.  Although you weren't perfect, you were damn good. We'll just be sure to keep you away from sledgehammers.  And tomato soup.", "assets/images/outcome8.png"),
+        new outcome("Leatherface", "The creme de la creme of horror baddies - this dude is a complete sociopath.  Chainsaws, cannibalism, meat hooks - this gentleman is not one to be trifled with - much like you with regards to your profound horror movie knowledge", "assets/images/outcome9.png"),
+        new outcome("Otis Driftwood", "Oh were you expecting Jason? Sorry to disappoint.  Otis Driftwood is king baddie - satanism, rituals and demon surgeons coupled with witty one-liners and amazing t-shirts - you are truly the best and your performance was a masterpiece.", "assets/images/outcome10.png"),
+    ];
+    //function to convert the array above to object notation
+    function outcome(name, explanation, image) {
+        this.name = name;
+        this.explanation = explanation;
+        this.image = image;
+    }
+
+    //variable used for the timer/countdown
+    var timer;
+
+    //Arrays to hold the actual correct answers and user's quiz answers
+    var correctAnswers = [];
+    var userAnswers = [];
+    
+    //Answer counters
+    var answersCorrect = 0;
+    var answersWrong = 0;
+    
+
 
     //Click the button to start the game.  This will run the setup function and star the timer
     $("#startGame").click(function() {
@@ -164,6 +185,9 @@ $(document).ready(function () {
         $("#correctAnswers").text(answersCorrect);
         $("#incorrectAnswers").text(answersWrong);
         $("#finalScore").text(tally + "%");
+        $("#your_villain").text(outcomeAnalysis[answersCorrect].name);
+        $("#villain_image").attr("src", outcomeAnalysis[answersCorrect].image);
+        $("h4#explanation").text(outcomeAnalysis[answersCorrect].explanation);
     }    
 
 });
